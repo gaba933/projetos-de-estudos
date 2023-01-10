3e2b6019620f65fab3eaf966a7a66ac086f47e84
gabriel <gabriel.h.v.d.o@gmail.com>
2023-01-10 15:18:53 -0300

    exercidos java
    tentativa de otimizar o menu

--- projeto java/Banco/src/main/java/com/mycompany/banco/AppBanco.java
+++ projeto java/Banco/src/main/java/com/mycompany/banco/AppBanco.java
@@ -27,15 +27,114 @@ public class AppBanco {
         GerenciarCliente gerCliente = new GerenciarCliente();
         GerenciarConta gerConta = new GerenciarConta();
 
+        tela_menuPrincipal(menuPrincipal, gerCliente, gerConta);
+
+    }
+
+    public  static  void tela_menuPrincipal(String menuPrincipal, GerenciarCliente gerCliente, GerenciarConta gerConta){
+
         int op =0;
         while (op!=5){
             op = Integer.parseInt(JOptionPane.showInputDialog(menuPrincipal) );
 
             switch (op){
                 case 0:{
+                    //codigo para cadastrar cliente
+
+                    Cliente cliente = tela_cadCliente();
+                    gerCliente.cadastrar(cliente);
+
+                    break;
+                }
+
+                case 1:{
                     //codigo para cadastrar conta
 
-                    Cliente cliente =
+                    int tipoConta =0;
+                    Conta conta = tela_cadConta(gerCliente, tipoConta);
+                    gerConta.cadastrar(conta);
+
+                    break;
+                }
+
+                case 2:{
+                    //codigo para cadastrar conta espacial
+
+                    int tipoConta =1;
+                    Conta conta = tela_cadConta(gerCliente, tipoConta);
+
+                    break;
+                }
+
+                case 3:{
+                    //Relatorios
+
+                    int oprelatorio = 0;
+
+                }
+            }
+        }
+    }
+
+    public static Cliente tela_cadCliente(){
+
+        String cpf;
+        String nome;
+        String endereco;
+        String telefone;
+
+        nome = JOptionPane.showInputDialog("digite o nome: ");
+        cpf = JOptionPane.showInputDialog("digite o spc: ");
+        endereco = JOptionPane.showInputDialog("digite o endereco: ");
+        telefone = JOptionPane.showInputDialog("digite o telefone: ");
+
+        return new Cliente(nome, cpf, endereco,telefone);
+    }
+
+    public static Conta tela_cadConta( GerenciarCliente gerCliente, int tipoConta ){
+
+        int numero;
+        String cpf;
+
+        Cliente cliente;
+
+        numero = Integer.parseInt(JOptionPane.showInputDialog("digite o numero da conta: ") );
+        cpf = JOptionPane.showInputDialog("digite o cpf do titular: ");
+
+        cliente = gerCliente.buscar(cpf);
+
+        if (tipoConta==0){
+            return  new Conta(numero, cliente);
+        } else {
+            return new ContaEspecial(numero, cliente);
+        }
+    }
+
+    public static void tela_relatorios(String menuRelatorios, GerenciarCliente gerCliente, GerenciarConta gerConta){
+
+        int op =0;
+        while (op!=5){
+            op = Integer.parseInt(JOptionPane.showInputDialog(menuRelatorios) );
+
+            switch (op){
+                case 0:{
+                    //lista os clientes
+
+                    JOptionPane.showMessageDialog(null, gerCliente.imprimir() );
+
+                    break;
+                }
+
+                case 1:{
+                    //lista as contas cadastradas
+
+                    JOptionPane.showMessageDialog(null, gerConta.imprimir() );
+
+                    break;
+                }
+
+                case 2:{
+                    //buscar conta com maior saldo
                 }
             }
         }
